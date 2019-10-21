959c0b3bf7cb522691d73974818f9cd8746938f4
Maxime Salomon <maxime@croissant.io>
2019-10-21 17:19:49 +0200

    update if_else

--- Python/if_else.py
+++ Python/if_else.py
@@ -39,9 +39,10 @@
 
 if n % 2 == 1:
     print("Weird")
-if n % 2 == 0 and n >= 2 and n <= 5:
-    print("Not Weird")
-if n % 2 == 0 and n >= 6 and n <= 20:
-    print("Weird")
-if n % 2 == 0 and n > 20:
-    print("Not Weird")
+else:
+    if n >= 2 and n <= 5:
+        print("Not Weird")
+    if n % 2 == 0 and n >= 6 and n <= 20:
+        print("Weird")
+    if n % 2 == 0 and n > 20:
+        print("Not Weird")
