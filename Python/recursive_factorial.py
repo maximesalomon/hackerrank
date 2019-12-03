f7b4345bcff6cbab87b8d4cedde523cc8c58b8fc
Maxime Salomon <maxime@croissant.io>
2019-12-03 16:57:38 +0100

    newb

--- Python/recursive_factorial.py
+++ Python/recursive_factorial.py
@@ -1,5 +1,5 @@
 def recursive_factorial(n):
-    if n == 0:
+    if n == 0: # a base case that specify when the recursion terminates
         return 1
-    return n * recursive_factorial(n - 1)
+    return n * recursive_factorial(n - 1) # rule that reduce all other cases towards a base case
     
