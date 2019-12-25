cacaef1346989f2892cc1b3483381f03fdbbe6e3
Maxime Salomon <maxime@croissant.io>
2019-12-25 23:12:49 +0100

    forEach + map array method

--- JavaScript/callback.js
+++ JavaScript/callback.js
@@ -1,19 +1,34 @@
-const elements = ['earth', 'wind', 'fire', 'water']
+const elements = ["earth", "wind", "fire", "water"];
 
 const showFirst = (arr, cb) => {
-    cb(arr[0]);
-}
+  cb(arr[0]);
+};
 
-showFirst(elements, (firstItem) => {
-    console.log(firstItem);
-})
+showFirst(elements, firstItem => {
+  console.log(firstItem);
+});
 
 //////////////////////
 
 const showLength = (arr, cb) => {
-    cb(arr.length);
-}
+  cb(arr.length);
+};
 
-showLength(elements, (lengthOfList) => {
-    console.log(lengthOfList)
-})
+showLength(elements, lengthOfList => {
+  console.log(lengthOfList);
+});
+
+//////////////////////
+
+elements.forEach((element, idx) => {
+  console.log(element, idx);
+});
+
+//////////////////////
+
+const newElements = elements.map((element, idx) => {
+  return `${element} ${idx}`;
+});
+
+console.log(newElements);
+console.log(elements);
