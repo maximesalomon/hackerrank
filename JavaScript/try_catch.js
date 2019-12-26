67f4b89448eb297e95823fcab627ff5a10cae18f
Maxime Salomon <maxime@croissant.io>
2019-12-27 00:05:16 +0100

    try catch

---
+++ JavaScript/try_catch.js
@@ -0,0 +1,11 @@
+function reverseString(s) {
+    try {
+        console.log(s.split("").reverse().join(""));
+    }
+    catch(error) {
+        console.log(error.message);
+        console.log(s)
+    }
+}
+
+reverseString("Maxime")
