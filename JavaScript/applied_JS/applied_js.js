0ac0d09e740e122635e33c6fc334eeed8e021a36
Maxime Salomon <maxime@croissant.io>
2020-01-02 10:16:11 +0100

    addEventListener

--- JavaScript/applied_JS/applied_js.js
+++ JavaScript/applied_JS/applied_js.js
@@ -1,1 +1,7 @@
 mainHeaderRed = document.querySelector('.main-header').style.color = 'red';
+
+const sayHello = () => {
+    alert("Hello 👋")
+}
+
+helloBtn = document.getElementById('hello-btn').addEventListener('click', sayHello)
