6466225be985896f07ba07763e2ec8ad5593a912
Maxime Salomon <maxime@croissant.io>
2020-01-08 19:59:15 +0100

    new-react-app hello, world 👋

---
+++ React/new-react-app/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello, world <span role="img" aria-label="hand-waving">👋</span></h1>
+    </div>
+  );
+}
+
+export default App;
