b9e3b2c46150efd476beac523695d9026951ecbe
Maxime Salomon <maxime@croissant.io>
2020-01-08 20:17:43 +0100

    add useState hook

--- React/new-react-app/src/App.js
+++ React/new-react-app/src/App.js
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-function App() {
+const App = ({name}) => {
+  const [isDay, setIsDay] = useState(true)
+
   return (
     <div className="App">
-      <h1>Hello, world <span role="img" aria-label="hand-waving">👋</span></h1>
+      <h1>Hello, {name} <span role="img" aria-label="hand-waving">👋</span></h1>
+      <h2>It's {new Date().toLocaleTimeString()}.</h2>
+      <p>It's {isDay === true ? 'Day' : 'Night'}</p>
+      <button onClick={() => setIsDay(true)}>Day</button>
+      <button onClick={() => setIsDay(false)}>Night</button>
     </div>
   );
 }
