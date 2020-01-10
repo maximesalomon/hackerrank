6039b1411cb7e73c6d075b837942920591196c76
Maxime Salomon <maxime@croissant.io>
2020-01-10 12:07:13 +0100

    prettified

--- React/light-on-and-off/src/App.js
+++ React/light-on-and-off/src/App.js
@@ -1,15 +1,20 @@
 import React, { useState } from "react";
 
 const white = "https://image.flaticon.com/icons/png/512/32/32177.png";
-const yellow = "https://i.pinimg.com/originals/92/94/ba/9294badee7b8f3d93fa9bc6c874641b2.png";
+const yellow =
+  "https://i.pinimg.com/originals/92/94/ba/9294badee7b8f3d93fa9bc6c874641b2.png";
 
 const App = () => {
   const [lightOn, setLightOn] = useState(true);
   return (
     <div onClick={() => setLightOn(!lightOn)} className="App">
-      {lightOn === false ? <img src={white} alt="lightbulb off"/> : <img src={yellow} alt="lightbulb on"/>}
+      {lightOn === false ? (
+        <img src={white} alt="lightbulb off" />
+      ) : (
+        <img src={yellow} alt="lightbulb on" />
+      )}
     </div>
   );
-}
+};
 
 export default App;
