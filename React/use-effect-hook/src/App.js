06610be5b6c523a4a54c468ccc9ac65d59acf63c
Maxime Salomon <maxime@croissant.io>
2020-01-11 15:37:23 +0100

    styled-components

--- React/use-effect-hook/src/App.js
+++ React/use-effect-hook/src/App.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import styled from 'styled-components'
 
 const App = () => {
   const [count, setCount] = useState(0);
@@ -8,13 +9,29 @@ const App = () => {
   });
 
   return (
-    <div>
+    <StyledDiv>
       <p>You clicked {count} times</p>
-      <button onClick={() => setCount(count + 1)}>
+      <StyledButton onClick={() => setCount(count + 1)}>
         Click me
-      </button>
-    </div>
+      </StyledButton>
+    </StyledDiv>
   );
 }
+
+const StyledDiv = styled.div`
+  background-color: lightgray;
+  padding: 50px;
+  height: 100px;
+  width: 200px;
+  margin: 200px auto;
+  border: 1px solid black;
+`;
+
+const StyledButton = styled.button`
+  height: 40px;
+  width: 80px;
+  background-color: white;
+  border: 1px solid black; 
+`
 
 export default App;
