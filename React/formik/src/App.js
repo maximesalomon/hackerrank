2b8cf6f016df4d1dd696b79de7c93a86497679a2
Maxime Salomon <maxime@croissant.io>
2020-01-13 16:55:51 +0100

    validating fields w/ formik

--- React/formik/src/App.js
+++ React/formik/src/App.js
@@ -1,25 +1,44 @@
 import React from 'react';
-import { Formik, Form, Field } from 'formik';
+import { Formik, Form, Field, ErrorMessage } from "formik";
+
+const validate = ({ username, password }) => {
+  const errors = {};
+  if (!username) {
+    errors.username = 'Please enter an username';
+  }
+  if (!username) {
+    errors.password = 'Please enter a password';
+  }
+  return errors;
+};
 
 const App = () => {
   return (
     <Formik
-      initialValues={{ name:'', password:'' }}
-      onSubmit={( values, tools ) => {
-        console.log(values, tools)
-        tools.resetForm()
-      }}>
+      validate={validate}
+      initialValues={{ username: '', password: '' }}
+      onSubmit={(values, tools) => {
+        console.log(values, tools);
+        tools.resetForm();
+      }}
+    >
       {props => {
         return (
           <Form>
-            <Field name='name' type='text' placeholder='Name'></Field>
-            <Field name='password' type='password' placeholder='Password'></Field>
+            <Field name='username' type='text' placeholder='Name'></Field>
+            <ErrorMessage name='username' component='div'/>
+            <Field
+              name='password'
+              type='password'
+              placeholder='Password'
+            ></Field>
+            <ErrorMessage name='password' component='div' />
             <input type='submit' />
           </Form>
-        )
+        );
       }}
-      </Formik>
+    </Formik>
   );
-}
+};
 
 export default App;
