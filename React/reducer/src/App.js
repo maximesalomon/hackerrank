b7d31383a3f036faba5e3d9c99bced4832a4785f
Maxime Salomon <maxime@croissant.io>
2020-01-15 23:02:23 +0100

    useReducer counter INCREMENT + DECREMENT

--- React/reducer/src/App.js
+++ React/reducer/src/App.js
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 
-function App() {
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'INCREMENT':
+      return state + 1;
+    case 'DECREMENT':
+      return state - 1;
+    default:
+      return state;
+  }
+};
+
+const App = () => {
+  const [count, dispatch] = useReducer(reducer, 0)
+
   return (
     <div className="App">
-
+      <p>Count = {count}</p>
+      <button onClick={() => dispatch({ type: 'INCREMENT'})}>Increment</button>
+      <button onClick={() => dispatch({ type: 'DECREMENT'})}>Decrement</button>
     </div>
   );
 }
