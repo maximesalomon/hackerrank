e41a65186fad9e5e5cd59d5d54d1a70f9ad1a146
Maxime Salomon <maxime@croissant.io>
2020-01-15 23:56:48 +0100

    useReducer todos + todoCount

--- React/use-reducer-todo/src/App.js
+++ React/use-reducer-todo/src/App.js
@@ -1,34 +1,59 @@
-import React, { useState, useReducer } from 'react';
+import React, { useState, useReducer } from "react";
 
 const reducer = (state, action) => {
   switch (action.type) {
-    case 'ADDTODO':
-      return {todos: [...state.todos, {text: action.text, completed: false }] };
-
+    case "ADDTODO":
+      return {
+        todos: [...state.todos, { text: action.text, completed: false }],
+        todoCount: state.todoCount + 1
+      };
+    case "DONE":
+      return {
+        todos: state.todos.map((t, idx) =>
+          idx === action.idx ? { ...t, completed: !t.completed } : t
+        ),
+        todoCount: state.todoCount
+      };
     default:
       return state;
   }
-}
-
-
+};
 
 const App = () => {
   const [text, setText] = useState();
 
-  const [{todos}, dispatch] = useReducer(reducer, {todos: [] })
+  const [{ todos, todoCount }, dispatch] = useReducer(reducer, {
+    todos: [],
+    todoCount: 0
+  });
 
   return (
     <div className="App">
-      <form onSubmit={e => {
-        e.preventDefault()
-        dispatch({type: 'ADDTODO', text})
-        setText('')
-      }}>
+      <form
+        onSubmit={e => {
+          e.preventDefault();
+          dispatch({ type: "ADDTODO", text });
+          setText("");
+        }}
+      >
         <input value={text} onChange={e => setText(e.target.value)}></input>
       </form>
-      <pre>{JSON.stringify(todos, null, 2)}</pre>
+      <p>TODO Count = {todoCount}</p>
+      <ul>
+        {todos.map((t, idx) => (
+          <li
+            key={t.text}
+            onClick={() => dispatch({ type: "DONE", idx })}
+            style={{
+              textDecoration: t.completed ? "line-through" : ""
+            }}
+          >
+            {t.text}
+          </li>
+        ))}
+      </ul>
     </div>
   );
-}
+};
 
 export default App;
